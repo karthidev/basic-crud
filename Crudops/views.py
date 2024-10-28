7640a4f927d719f74e26b72e89413f1d99a99f38
Karthick <karthidev002@gmail.com>
2024-10-28 22:00:18 +0530

    first_commit

---
+++ Crudops/views.py
@@ -0,0 +1,31 @@
+
+# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from .forms import ItemForm
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'Cruds/item_list.html', {'items': items})
+
+def item_create(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('item_list')
+    return render(request, 'Cruds/item_form.html', {'form': form})
+
+def item_update(request, id):
+    item = get_object_or_404(Item, id=id)
+    form = ItemForm(request.POST or None, instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('item_list')
+    return render(request, 'Cruds/item_form.html', {'form': form})
+
+def item_delete(request, id):
+    item = get_object_or_404(Item, id=id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'Cruds/item_confirm_delete.html', {'item': item})
